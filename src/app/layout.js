b3bd76c4b984179eb59d9d1d4ebd1dc5b1adb62d
iMoloy <moloykrishnapaul@gmail.com>
2026-04-30 08:46:03 +0600

    Fixed hydration mismatch error for Vercel build

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,14 @@ import NavbarComponent from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "LuminaTiles Gallery",
+  title: "Tiles Gallery",
   description: "A website to showcase a tile gallery.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light">
-      <body className="antialiased">
+    <html lang="en" data-theme="light" suppressHydrationWarning>
+      <body className="antialiased" suppressHydrationWarning>
         <NavbarComponent />
         <main className="min-h-[calc(100vh-200px)]">{children}</main>
         <Footer />
